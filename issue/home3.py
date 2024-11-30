08db3ec38f441dd407955daf058620fae0b5453d
liweii99 <liweii9988@gmail.com>
2024-11-30 02:42:29 +0000

    liwei

--- issue/home3.py
+++ issue/home3.py
@@ -3,10 +3,12 @@ class BMI():
         self.name=name
         self.height = height
         self.weight = weight
+
     def get_BMI(self)->float:
         BMI_issue = round(self.weight/ ((self.height/100)**2),2)
         return BMI_issue
-
+ # instance method
+    #def get_status(self,bmi:float)->str:
     def get_status(self)->str:
         bmi = self.get_BMI()
         if bmi >= 35 :
@@ -21,17 +23,22 @@ class BMI():
             return'正常範圍'
         else :
             return'體重過輕'
-   
-while(True):
-    try:
-        name = input("姓名:")
-        height = float(input("請輸入身高(公分):"))
-        weight = float(input("請輸入體重(公斤):"))
-        p1 = BMI(name=name,height=height,weight=weight)   
-        break
-    except Exception:
-        print('輸入格式錯誤,請重新輸入!')
-    
-print(f"您的BMI值是{p1.get_BMI()}\n您的體重是{p1.weight}\n您的身體質量指數屬於")
-print(p1.get_status())
-print("程式結束")
+        
+def main():  
+
+    while(True):
+        try:
+            name = input("姓名:")
+            height = float(input("請輸入身高(公分):"))
+            weight = float(input("請輸入體重(公斤):"))
+            p1 = BMI(name=name,height=height,weight=weight)   
+            break
+        except Exception:
+            print('輸入格式錯誤,請重新輸入!')
+        
+    print(f"您的BMI值是{p1.get_BMI()}\n您的體重是{p1.weight}\n您的身體質量指數屬於")
+    print(p1.get_status())
+    print("程式結束")
+
+if __name__ == '__main__':
+    main()
